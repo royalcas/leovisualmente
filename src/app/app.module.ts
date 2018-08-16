a85409bb1520003c626e808b2ccc9f36403b9054
royalcas <roger.alvarez.c@hotmail.com>
2018-08-15 23:12:56 -0500

    Initial 360 view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { PanoComponent } from './pano/pano.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    PanoComponent
   ],
   imports: [
     BrowserModule
